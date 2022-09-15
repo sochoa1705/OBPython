958d6e94ecd86016c1403890e082426e0d1cce4a
saochoa1 <saochoa1@espe.edu.ec>
2022-09-15 17:34:45 -0500

    Solución ejercicio 3

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -1,2 +1,10 @@
 import math
+weight = input('Ingrese su peso(kg): ')
+height = input('Ingrese su estatura(cm): ')
+heightMeter = int(height)/100
+
+imc = int(weight)/math.pow(heightMeter,2)
+
+print('Su IMC es de: {:.2f}' .format(imc))
+
 
